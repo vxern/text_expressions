daa95cf88861c04f2d00a52cb91eb2d75e0a3202
vxern <vxern@wordcollector.co.uk>
2023-01-15 15:22:08 +0000

    misc: Remove unused `orElse` parameter.

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -3,19 +3,13 @@
 extension NullSafeAccess<E> on Iterable<E> {
   /// Returns the first element that satisfies the given predicate [test].
   ///
-  /// If no elements satisfy [test], the result of invoking the [orElse]
-  /// function is returned.
-  ///
   /// Unlike `Iterable.firstWhere()`, this function defaults to returning `null`
   /// if an element is not found.
-  E? firstWhereOrNull(bool Function(E) test, {E Function()? orElse}) {
+  E? firstWhereOrNull(bool Function(E) test) {
     for (final element in this) {
       if (test(element)) {
         return element;
       }
-    }
-    if (orElse != null) {
-      return orElse();
     }
     return null;
   }
